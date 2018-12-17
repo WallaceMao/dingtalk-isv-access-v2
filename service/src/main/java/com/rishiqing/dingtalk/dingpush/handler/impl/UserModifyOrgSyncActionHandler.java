50598977255a5454bd7013b39cc59bf7c3b9dc31
Wallace Mao <maowenqiang0752@163.com>
2018-12-17 16:02:37 +0800

    fix: user_modified_org, create user when user not found

--- service/src/main/java/com/rishiqing/dingtalk/dingpush/handler/impl/UserModifyOrgSyncActionHandler.java
+++ service/src/main/java/com/rishiqing/dingtalk/dingpush/handler/impl/UserModifyOrgSyncActionHandler.java
@@ -19,25 +19,41 @@ import java.util.Date;
 public class UserModifyOrgSyncActionHandler implements SyncActionHandler {
     @Autowired
     private StaffService staffService;
+    @Autowired
+    private CorpStaffManageService corpStaffManageService;
+
     /**
-     * @link https://open-doc.dingtalk.com/microapp/ln6dmh/troq7i
-     subscribe_id  ： 套件suiteid加下划线0
-     copp_id : 开通套件微应用的企业corpid
-     biz_id             ： 员工userId
-     biz_data         ：数据为如下两种 Json格式：
-     企业增加或修改员工，字段值来自于开放平台接口user/get 。
-     "syncAction": "user_add_org"，表示企业增加员工事件之后的员工信息
-     "syncAction": "user_modify_org" ,表示企业修改员事件之后的员工信息
-     "syncAction": "user_dept_change" ，表示企业修改员工所在部门事件之后的员工信息
-     "syncAction": "user_role_change"，表示企业修改员工所在角色(包括管理员变更)事件之后的员工信息
      * @param data
+     * @link https://open-doc.dingtalk.com/microapp/ln6dmh/troq7i
+     * subscribe_id  ： 套件suiteid加下划线0
+     * copp_id : 开通套件微应用的企业corpid
+     * biz_id             ： 员工userId
+     * biz_data         ：数据为如下两种 Json格式：
+     * 企业增加或修改员工，字段值来自于开放平台接口user/get 。
+     * "syncAction": "user_add_org"，表示企业增加员工事件之后的员工信息
+     * "syncAction": "user_modify_org" ,表示企业修改员事件之后的员工信息
+     * "syncAction": "user_dept_change" ，表示企业修改员工所在部门事件之后的员工信息
+     * "syncAction": "user_role_change"，表示企业修改员工所在角色(包括管理员变更)事件之后的员工信息
      */
     @Override
     public void handleSyncAction(OpenSyncBizDataVO data) {
         JSONObject json = JSONObject.parseObject(data.getBizData());
         String corpId = data.getCorpId();
+        String userId = data.getBizId();
+
+        // 如果errcode不为0，例如50002，说明userId不在可见范围之内，那么此时删除该用户
+        if (json.containsKey("errcode") && json.getLong("errcode") > 0) {
+            staffService.deleteUserAndSubtractCount(corpId, userId);
+            return;
+        }
         CorpStaffVO corpStaffVO = SuiteDbCheckConverter.json2CorpStaff(json);
         corpStaffVO.setCorpId(corpId);
-        staffService.updateAndPushCorpStaff(corpStaffVO, new Date().getTime());
+        // 这里要判断是不是userId是不是存在，如果不存在的话要走保存方法，如果存在的话走更新
+        CorpStaffVO dbStaff = corpStaffManageService.getCorpStaffByCorpIdAndUserId(corpId, userId);
+        if (dbStaff == null) {
+            staffService.saveCorpStaffAndAddCount(corpStaffVO, new Date().getTime());
+        } else {
+            staffService.updateAndPushCorpStaff(corpStaffVO, new Date().getTime());
+        }
     }
 }
